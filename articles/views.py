698c8e763fbcd8bf81b36a1dee41037ff99766cc
Sherzod <sherzodakramov1999@gmail.com>
2023-03-17 15:19:20 +0500

    Change .gitignore file

--- articles/views.py
+++ articles/views.py
@@ -9,6 +9,7 @@ from .models import Article
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 # Create your views here.
 
 
@@ -16,8 +17,8 @@ def email(request):
     subject = 'Thank you for registering to our site'
     message = ' it  means a world to us '
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['receiver@gmail.com',]
-    send_mail( subject, message, email_from, recipient_list )
+    recipient_list = ['receiver@gmail.com', ]
+    send_mail(subject, message, email_from, recipient_list)
     return redirect('redirect to a new page')
 
 
@@ -60,6 +61,6 @@ class ArticleCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-    # user superuser ekanini tekshirish
+    # checking user is superuser
     def test_func(self):
         return self.request.user.is_superuser
